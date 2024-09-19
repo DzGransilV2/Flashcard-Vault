e728291c303398e19774442aa47b2e7819ba1a8a
DzGransilV2 <gransildsouza2@gmail.com>
2024-09-19 08:27:52 +0530

    #updateStructureWithCSS

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,7 +5,7 @@ import { Tabs } from 'expo-router'
 const TabsLayout = () => {
   return (
     <Tabs>
-        <Tabs.Screen name="home" options={{headerShown:false}} />
+        <Tabs.Screen name="index" options={{headerShown:false}} />
         <Tabs.Screen name="quiz" options={{headerShown:false}} />
         <Tabs.Screen name="create" options={{headerShown:false}} />
         <Tabs.Screen name="edit" options={{headerShown:false}} />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <Text className="font-black text-red-800">Home</Text>
     </SafeAreaView>
   )
 }
